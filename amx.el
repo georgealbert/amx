37f9c7ae55eb0331b27200fb745206fc58ceffc0
Ryan C. Thompson <rct@thompsonclan.org>
2021-03-04 20:18:22 -0500

    Version 3.4

--- amx.el
+++ amx.el
@@ -9,7 +9,7 @@
 ;; Maintainer: Ryan C. Thompson <rct@thompsonclan.org>
 ;; URL: http://github.com/DarwinAwardWinner/amx/
 ;; Package-Requires: ((emacs "24.4") (s "0"))
-;; Version: 3.3
+;; Version: 3.4
 ;; Keywords: convenience, usability, completion
 
 ;; This file is not part of GNU Emacs.
